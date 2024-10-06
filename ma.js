6f750c16e831d36855daada80709cf4fe6704561
Mohamed Elshaowky <elshoky330@gmail.com>
2024-10-06 10:36:12 +0300

    first commit

---
+++ ma.js
@@ -0,0 +1,8 @@
+// document.querySelectorAll(".navbar a").forEach(function (link) {
+//   link.addEventListener("click", function () {
+//     var navcollapse = document.querySelector("collapse");
+//     if (navcollapse.classList.contains === "show") {
+//       navcollapse.classList.remove("show");
+//     }
+//   });
+// });
